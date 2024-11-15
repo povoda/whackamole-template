3289635062206aee6cebf087bc9629714066aff1
AnastasiiaP <anastasiia.povod@ufl.edu>
2024-11-15 15:58:26 -0500

    implement mole hitting logic

--- whackamole.py
+++ whackamole.py
@@ -1,7 +1,27 @@
 import pygame
+import random
 
+
+def draw_grid(pygame, screen, line_color):
+    for i in range(20):
+        pygame.draw.line(screen, line_color, (32 * i, 0), (32 * i, 512))
+
+    for i in range(16):
+        pygame.draw.line(screen, line_color, (0, 32 * i), (640, 32 * i))
+
+def draw_mole(screen, mole_image, x, y):
+    # x:0-19 y:0-15
+    x_coord = x * 32 + 2
+    y_coord = y * 32 + 1
+    screen.blit(mole_image, mole_image.get_rect(topleft=(x_coord ,y_coord)))
+
+def is_mole_hit(user_x, user_y, mole_x, mole_y):
+    return user_x // 32 == mole_x and user_y // 32 == mole_y
 
 def main():
+    current_x = 0
+    current_y = 0
+
     try:
         pygame.init()
         # You can draw the mole with this snippet:
@@ -9,12 +29,24 @@ def main():
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+        line_color = "black"
+
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = event.pos
+                    print("X=", x, ", Y=", y)
+                    if is_mole_hit(x, y, current_x, current_y):
+                        current_x = random.randrange(0,20)
+                        current_y = random.randrange(0,16)
+
+
             screen.fill("light green")
+            draw_grid(pygame, screen, line_color)
+            draw_mole(screen, mole_image, current_x, current_y)
             pygame.display.flip()
             clock.tick(60)
     finally:
